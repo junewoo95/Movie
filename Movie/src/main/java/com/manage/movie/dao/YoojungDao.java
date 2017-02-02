f4b62d22f24938531384932fbac3b4556a000994
yoojung <dldbwjd33@naver.com>
2017-02-02 15:23:51 +0900

    hi

---
+++ Movie/src/main/java/com/manage/movie/dao/YoojungDao.java
@@ -0,0 +1,56 @@
+package com.manage.movie.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.manage.movie.vo.MovieEntity;
+import com.manage.movie.vo.NoticeEntity;
+import com.manage.movie.vo.TheaterEntity;
+
+
+@Repository
+public class YoojungDao{
+	
+	@Autowired
+	SqlSessionTemplate sqlSession;
+
+	public List<MovieEntity> movieselectList() throws Exception{
+			return sqlSession.selectList("movieDao.movieorderby");
+	}
+	
+	public List<MovieEntity> selectMovieGrade() throws Exception{
+		return sqlSession.selectList("movieDao.selectMovieGrade");
+	}
+	
+	public List<HashMap> selectTheaterMovieList(String th_name) throws Exception{
+		return sqlSession.selectList("movieDao.selectTheaterMovieList", th_name);
+	}
+	
+	public List<HashMap> selectTheaterMovieListTime(String th_name) throws Exception{
+		return sqlSession.selectList("movieDao.selectTheaterMovieListTime", th_name);
+	}
+
+/*	public List<NoticeEntity> noticewhereall() throws Exception{
+		return sqlSession.selectList("movieDao.noticewhereall");
+	}*/
+	
+	public List<TheaterEntity> selectDoList() throws Exception{
+		return sqlSession.selectList("movieDao.selectDoLocation");
+	}
+	public List<TheaterEntity> selectTheaterList(String th_do) throws Exception{
+		return sqlSession.selectList("movieDao.selectTheaterLocation", th_do);
+	}
+	
+	public List<MovieEntity> selectList() throws Exception{
+		return sqlSession.selectList("MovieDao.selectAllList");
+	}
+	
+	public MovieEntity selectmovieInfo(MovieEntity movie) throws Exception{
+		return sqlSession.selectOne("MovieDao.selectMovie", movie);
+	}
+
+}
---
+++ Movie/src/main/java/com/manage/movie/service/YoojungService.java
@@ -0,0 +1,71 @@
+package com.manage.movie.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.manage.movie.dao.YoojungDao;
+import com.manage.movie.vo.MovieEntity;
+import com.manage.movie.vo.NoticeEntity;
+import com.manage.movie.vo.TheaterEntity;
+
+@Service
+public class YoojungService {
+
+	@Autowired
+	private YoojungDao movieDao;
+
+	public List<MovieEntity> movieselectList() throws Exception{
+
+		List<MovieEntity> rtnList = movieDao.movieselectList();
+
+		return rtnList;
+	}
+	
+	public List<HashMap> selectTheaterMovieList(String th_name) throws Exception{
+
+		List<HashMap> rtnList = movieDao.selectTheaterMovieList(th_name);
+
+		return rtnList;
+	}
+	
+	public List<HashMap> selectTheaterMovieListTime(String th_name) throws Exception{
+
+		List<HashMap> rtnList = movieDao.selectTheaterMovieListTime(th_name);
+
+		return rtnList;
+	}
+	
+/*	public List<NoticeEntity> noticewhereall() throws Exception{
+
+		List<NoticeEntity>rtnList = movieDao.noticewhereall();
+
+		return rtnList;
+	}*/
+	
+	public List<TheaterEntity> selectDoList() throws Exception{
+
+		List<TheaterEntity>rtnList = movieDao.selectDoList();
+
+		return rtnList;
+	}
+	public List<TheaterEntity> selectTheaterList(String th_do) throws Exception{
+
+		List<TheaterEntity>rtnList = movieDao.selectTheaterList(th_do);
+
+		return rtnList;
+	}
+
+	public List<MovieEntity> getAllList() throws Exception{
+
+		List<MovieEntity>rtnList = movieDao.selectList();
+
+		return rtnList;
+	}
+	
+	public MovieEntity selectMovieInfo(MovieEntity movie) throws Exception{
+		return movieDao.selectmovieInfo(movie);
+	}
+}
